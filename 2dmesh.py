5117801a1611b65d1c178cbf9ee2c40641eb8eaa
morelandfm <morelandfm50@gmail.com>
2026-03-17 16:02:29 -0700

    Add files via upload
    Mdae changes

--- 2dmesh.py
+++ 2dmesh.py
@@ -1,15 +1,32 @@
+#!/home/devpi/Desktop/2dMesh/2dvenv/bin python
 from polygenerator import random_polygon
 import matplotlib.pyplot as plt
+import pygmsh
 
-vertices = random_polygon(num_points=10)
+def coordsTo3dLol(xcoords, ycoords):
+    if len(xcoords) != len(ycoords):
+        print("Error, number of x coords and y coords not equal")
+    listOfCoords = []
+    for i in range(len(xcoords)):
+        coords = [xcoords[i], ycoords[i], 0]
+        listOfCoords.append(coords)
+    return(listOfCoords)
+
+
+
+
+
+vertices = random_polygon(num_points=4)
 
 x_coords, y_coords = zip(*vertices)
 x_coords = list(x_coords) + [x_coords[0]]
 y_coords = list(y_coords) + [y_coords[0]]
 
+totCoords = coordsTo3dLol(x_coords, y_coords)
 plt.plot(x_coords, y_coords, marker = 'o')
 plt.title("Random Test")
 plt.xlabel("X Coords")
 plt.ylabel("Y Coords")
 plt.grid(True)
 plt.show()
+print(totCoords)
