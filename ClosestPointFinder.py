c79e2b270ec827e02810c1b61baec802ef78868f
morelandfm <morelandfm50@gmail.com>
2026-03-27 05:33:01 -0700

    Update ClosestPointFinder.py
    Added funcs

--- ClosestPointFinder.py
+++ ClosestPointFinder.py
@@ -6,6 +6,7 @@ from numpy.linalg import norm
 from scipy.spatial.transform import Rotation
 from mpl_toolkits.mplot3d import Axes3D
 
+#Takes a list of lists of lists and turns it into a list of points and returns that list
 def pointsList(info):
     pointList = []
     for group in info:
@@ -13,6 +14,75 @@ def pointsList(info):
             for pt in plane:
                 pointList.append(pt)   
     return(pointList) 
+
+#Takes a list of points, determines their magnitude and orders them
+#in the size of their magnitude with respect to the origin
+#Need to change this to distance from point
+def distanceFromOrigin(coords):
+    magnitude = []
+    for point in coords:
+        mag = np.linalg.norm(point)
+        magnitude.append(mag)
+    zipped = zip(magnitude, coords)
+    sortedZip = sorted(zipped, key=lambda x: x[0], reverse=True)
+    sortedMags, sortedCoords = zip(*sortedZip)
+    return(sortedCoords)
+
+def distanceFromPoint(initialPoint, coords):
+    if initialPoint == 0:
+        magnitude = []
+        for point in coords:
+            mag = np.linalg.norm(point)
+            magnitude.append(mag)
+        zipped = zip(magnitude, coords)
+        sortedZip = sorted(zipped, key=lambda x: x[0], reverse=True)
+        sortedMags, sortedCoords = zip(*sortedZip)
+        return(sortedCoords)
+    vectList = []
+    for point in coords:
+        vect = [(initialPoint[0]-point[0], initialPoint[1]-point[1]), initialPoint[2]-point[2]]
+        vectList.append(vect)
+    vectMag = []
+    for point in vectList:
+        mag = np.linalg.norm(point)
+        vectMag.append(mag)
+    zipped = zip(vectMag, coords)
+    sortedZip = sorted(zipped, key=lambda x: x[0], reverse=True)
+    sortedMags, sortedCoords = zip(*sortedZip)
+    return(sortedCoords)
+
+
+
+def connectedCoords(numConns, initialPoint, coords):
+
+    return()
+
+def planeMaker(coords, connectedCoords, planeCoords):
+    #Determining if the function has finished and will break out of the recursion
+    if len(coords) == 0:
+        return planeCoords
+    #Statement for making sure that there are enough points remaining that can be connected with the random number generated for connected points
+    if len(coords) < 7:
+        #connect remaining points
+        return
+    
+    #First things first, we need to determine everything's distance from the origin
+    newCoords = distanceFromOrigin(coords)
+    #Make a random number of points for the chosen point to connect to
+    randConnectedPoints = random.randint(4, 7)
+    #Take our first point
+    pointToBeConnected = coords[0]
+    #Remove chosen point from the coords list
+    del coords[0]
+    #Make a list for the points to be connected
+    #Needs to find the number of points closest to it based off the random int pulled
+    #Use another func to do this
+    #Func needs to take in the random number, the list of coords, and the connected coord
+
+
+
+    return(coords)
+
 
 """All three inputs are list, this is so that we can do
 recursion with the function. connectedCoords and coordPlanes are both
@@ -26,4 +96,5 @@ def findClosestPoints(coords, connectedCoords, cordPlanes):
     if len(coords) < 10:
         return(cordPlanes)
     point = []
+    
     
